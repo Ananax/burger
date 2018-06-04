99c5a53571542c5606973eb9e672a2590789ee6b
Baran Birol <baranbirol@barans-mbp.lan>
2018-06-03 21:58:15 -0700

    pushing up initial files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('In-n-out Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Habit Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Five Guys Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Shake Shack Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Nations Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('McDonalds Burger', false);
