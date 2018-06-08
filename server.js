f79cfbc62dc9023f55b5086768039466ee7c0f79
Baran Birol <baranbirol@barans-mbp.lan>
2018-06-08 09:37:03 -0700

    last bit of changes

--- server.js
+++ server.js
@@ -1,2 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const port = 3000 || env.process.port;
+
+const app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(process.cwd() + '/public'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+
+// Set Handlebars.
+var exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require('./controllers/burger_controllers');
+app.use('/', routes);
+
+
+app.listen(port);
